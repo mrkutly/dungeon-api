ab56fed20e7ab40a3306d1e3ec59ee88c05ceaa8
mark sauer-utley <mark.utley.713@gmail.com>
2019-12-27 13:03:41 -0500

    first commit

---
+++ build/config/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PORT = process.env.PORT || 3001;
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { PORT } from './config/index';
-
-const app = express();
-
-app.get('/', (req, res) => res.send('hello'));
-
-app.listen(PORT, (): void => console.log(`Running on port ${PORT}`));
