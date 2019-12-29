151a71bcb335910f19107d9814fbd0963293c5b9
mark sauer-utley <mark.utley.713@gmail.com>
2019-12-29 18:53:01 -0500

    update swagger configs for heroku

--- src/middleware/apiDocs.ts
+++ src/middleware/apiDocs.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../config/swagger.json";
 
-export const handleAPIDocs = (router: Router) => {
+export const handleAPIDocs = (router: Router): void => {
   router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
--- src/middleware/errorHandlers.ts
+++ src/middleware/errorHandlers.ts
@@ -2,19 +2,19 @@ import { Request, Response, NextFunction, Router } from "express";
 import * as ErrorHandler from "../utils/ErrorHandler";
 
 const handle404Error = (router: Router): void => {
-  router.use((req: Request, res: Response) => {
+  router.use((req: Request, res: Response): void => {
     ErrorHandler.notFoundError();
   });
 };
 
 const handleClientError = (router: Router): void => {
-  router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  router.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
     ErrorHandler.clientError(err, res, next);
   });
 };
 
 const handleServerError = (router: Router): void => {
-  router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  router.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
     ErrorHandler.serverError(err, res, next);
   });
 };
